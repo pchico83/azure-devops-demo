1f890b6d1b8ff6ee50c68a3c871f58a0556d3750
Pablo Chico de Guzman <Pablo@Pablos-MacBook-Pro.local>
2015-12-09 20:17:48 +0100

    Hola Mundo

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def hello():
     except RedisError:
         visits = "<i>counter disabled. Cannot connect to Redis.</i>"
 
-    html = "<h3>Hello World!</h3>" \
+    html = "<h3>Hola Mundo!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<br/>"
 
